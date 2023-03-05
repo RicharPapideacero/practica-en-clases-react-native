1d3092d691e247398e9f2fe860883e636a70a423
Richar Edward Iglesias Alquicira <79646584+RicharPapideacero@users.noreply.github.com>
2023-03-05 14:13:30 -0800

    Actividad 5
    La funcionalidad de la vista es permitir la captura de las tres materias, dar clic en el
    Botón Inscribirse, Desactivar Botón y mostrar las tres materias en una sección de
    texto.

--- Actividad4/App.js
+++ Actividad4/App.js
@@ -1,67 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const [activo, setActivo] = useState(true);
   const [materia1, setMateria1] = useState("Ingresa tu primer materia");
   const [materia2, setMateria2] = useState("Ingresa tu segunda materia");
   const [materia3, setMateria3] = useState("Ingresa tu tercera materia");
-  const texto = "Tus materias";
+  const [inscrito, setInscrito] = useState("");
+  const [texto, setTexto] = useState("");
   const a = "\n" + materia1 + "\n" + materia2 + "\n" + materia3;
-  const Materias= (props)=>{
-    return (
-      <View> 
-        <Text>{props.nombre}</Text>
-      </View>
-    );
-  }
-
-  const getMaterias = () =>{
-    alert('Inscrito\n' 
-            + materia1 +"\n"
-            + materia2 +"\n"
-            + materia3);    
-            
-  }
+  
 
   return (
     <View style={styles.container}>
-      
+      <Text style={styles.titulodisenio}>Registro de Materias</Text>
+
       <TextInput
         style= {styles.cajatexto}
         onChangeText={(newText) => setMateria1(newText)}
-        defaultValue={materia1}
-      />
-      
-      <Text></Text>
+        placeholder= {materia1}
 
+      />
       <TextInput
         style= {styles.cajatexto}
         onChangeText={(newText) => setMateria2(newText)}
-        defaultValue={materia2}
+        placeholder= {materia2}
+
       />
-
-      <Text></Text>
-
       <TextInput
         style= {styles.cajatexto}
         onChangeText={(newText) => setMateria3(newText)}
-        defaultValue={materia3}
+        placeholder= {materia3}
       />
-      
-      <Text></Text>
 
       <Button
-        title='Inscribir'
-        /*onPress={getMaterias}*/
-        onPress={(getMaterias)
-        }        
+        style={styles.estilobutton}
+        color="#8a2be2"
+        onPress={()=>{
+          setActivo(false);
+          setTexto("Te inscribiste a:")
+          setInscrito(a);
+        }}
+        disabled={(!activo)}
+        title = {activo ? "Inscribir" : "Inscrito"}
       />
-    
-      <Text></Text>
 
-      <Text>{texto}</Text>
-      <Materias nombre={a} />
+      <Text style={styles.resultadodisenio}>{texto}</Text>
+      <Text style={styles.resultadomateriasdisenio}>{inscrito}</Text>
 
       <StatusBar style="auto" />
     </View>
@@ -80,5 +66,29 @@ const styles = StyleSheet.create({
     borderColor: "gray",
     borderWidth: 1,
     minWidth: 200,
+    borderRadius:30,
+    padding:10,
+    margin:10
+  },
+  titulodisenio:{
+    height:60,
+    color:"#c6ce00",
+    fontSize:20
+  },
+  resultadodisenio:{
+    height:30,
+    color:"#8a2be2",
+    fontSize:20
+  },
+  resultadomateriasdisenio:{
+    height:100,
+    color:"#0000ff",
+    fontSize:15,
+    fontStyle: 'italic'
+  },
+  estilobutton:{
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10
   }
 });
